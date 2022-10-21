3e7f071f52c0e28e9173aee87b3057cba6ba635d
fernando <fernandoreal.valentim@gmail.com>
2022-10-21 10:24:33 -0300

    a

--- lab-ds/src/App.tsx
+++ lab-ds/src/App.tsx
@@ -1,97 +1,73 @@
-import './styles/global.css'
-import reactLogo from "./react-orange.svg"
+import "./styles/global.css";
+import reactLogo from "./react-orange.svg";
 
-import maskPhone from "./utils/maskPhone.js"
+/* import maskPhone from "./utils/maskPhone.js"
+ */
 
+import { Button } from "./components/Button";
+import { Heading } from "./components/Heading";
+import { Text } from "./components/Text";
+import Input from "./components/Input";
 
-import { Button } from './components/Button'
-import { Heading } from './components/Heading'
-import { Text } from './components/Text'
-import { TextInput } from './components/Input'
+import { Envelope } from "phosphor-react";
+import { User } from "phosphor-react";
+import { LockSimple } from "phosphor-react";
+import { CheckboxComp } from "./components/Checkbox";
+import { Phone } from "phosphor-react";
 
-import { Envelope } from 'phosphor-react'
-import { User } from 'phosphor-react'
-import { LockSimple } from 'phosphor-react'
-import { CheckboxComp } from './components/Checkbox'
-import { Phone } from "phosphor-react"
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useEffect } from "react";
 
+export const App = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = () => {
+    console.log("!");
+  };
 
-
-export function App() {
   return (
-    <section className='w-screen h-screen flex-col bg-gray-900 flex items-center justify-center' >
-      <header className='flex flex-col items-center  mb-3'>
-        <img src={reactLogo} className="w-20 animate-spin-slow"/>
-          <Heading  asChild={false}  size={"md"}>
-            Ignite Lab
-          </Heading>
-          <Text asChild={false}  size={"md"}>
-            Registration form
-          </Text>
+    <section className="w-screen h-screen flex-col bg-gray-900 flex items-center justify-center">
+      <header className="flex flex-col items-center  mb-3">
+        <img src={reactLogo} className="w-20 animate-spin-slow" />
+        <Heading asChild={false} size={"md"}>
+          Ignite Lab
+        </Heading>
+        <Text asChild={false} size={"md"}>
+          Registration form
+        </Text>
       </header>
 
-      <form className='flex flex-col gap-1 items-stretch w-full max-w-sm'>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-1 items-stretch w-full max-w-sm"
+      >
         <label htmlFor="name" className="flex flex-col gap-2">
-          <Heading size={'xs'} asChild={false} >
+          <Heading size={"xs"} asChild={false}>
             Enter your name
           </Heading>
-          <TextInput.Root>
-            <TextInput.Icon > 
-              <User/>
-            </TextInput.Icon>
-              <TextInput.Input  type="text" id="name" placeholder='Your name'>
-            </TextInput.Input>
-          </TextInput.Root>
-        </label>
-        <label htmlFor="email" className="flex flex-col gap-2">
-          <Heading size={'xs'} asChild={false} >
-            Enter your e-mail address
-          </Heading>
-          <TextInput.Root>
-            <TextInput.Icon > 
-              <Envelope/>
-            </TextInput.Icon>
-              <TextInput.Input  type="email" id="email" placeholder='Your e-mail address'>
-            </TextInput.Input>
-          </TextInput.Root>
-        </label>
-        <label htmlFor="password" className="flex flex-col gap-2">
-          <Heading size={'xs'} asChild={false} >
-            Enter your password
-          </Heading>
-          <TextInput.Root>
-            <TextInput.Icon > 
-              <LockSimple/>
-            </TextInput.Icon>
-              <TextInput.Input  type="password" id="password" placeholder='Password must have +8 numbers'>
-            </TextInput.Input>
-          </TextInput.Root>
-        </label>
-        <label htmlFor="phone" className="flex flex-col gap-2">
-          <Heading size={'xs'} asChild={false} >
-            Enter your phone
-          </Heading>
-          <TextInput.Root>
-            <TextInput.Icon > 
-              <Phone/>
-            </TextInput.Icon>
-              <TextInput.Input  type="tel" id="phone" placeholder='(88) 8888-8888' onChange={e => maskPhone(e)}>
-            </TextInput.Input>
-          </TextInput.Root>
-        </label>
-        <label htmlFor='agree' className='flex items-center gap-2 mt-2'>
-          <CheckboxComp/>
-          <Text size='sm' className="text-gray-200" asChild={false}>I agree to the terms and conditions</Text>
+
+          <Input
+            
+            type={"text"}
+            placeholder={""}
+            {...register("firstName", { required: true, maxLength: 20 })}
+          />
         </label>
 
-       <Button type='submit' text='Sign in'/>
-      </form>   
+        <label htmlFor="agree" className="flex items-center gap-2 mt-2">
+          <CheckboxComp />
+          <Text size="sm" className="text-gray-200" asChild={false}>
+            I agree to the terms and conditions
+          </Text>
+        </label>
 
-      <footer className='flex flex-col items-center gap-4 mt-5 '>
-        <a href="#" className='text-gray-400 underline hover:text-gray-200'>Forgot your password?</a>      
-      </footer>   
+        <Button type="submit" text="Sign in" />
+      </form>
+
+      <footer className="flex flex-col items-center gap-4 mt-5 ">
+        <a href="#" className="text-gray-400 underline hover:text-ray-200">
+          Forgot your password?
+        </a>
+      </footer>
     </section>
-  )
-}
-
- 
+  );
+};
--- lab-ds/src/components/Input.tsx
+++ lab-ds/src/components/Input.tsx
@@ -1,47 +1,27 @@
-import { Slot } from '@radix-ui/react-slot';
-import { clsx } from 'clsx' ;
-import { InputHTMLAttributes, ReactNode } from 'react';
+import React from 'react'
 
-export interface InputRootProps {
-  children: ReactNode;
-}
-
-function InputRoot(props: InputRootProps) {
-return (
-  <div className='h-10 flex items-center gap-3 px-3 rounded w-full  bg-gray-800 focus-within:ring-1 ring-orange-500'>
-    {props.children}
-  </div>
-)
-}
-
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
-
-function Input(props: InputProps) {
-  return (
-    <input autoComplete='off'
-    className='bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none' {...props}/>
+type Props = {
+    name:string
     
-  )
+    type: string
+    placeholder: string
 }
 
+const Input: React.FC<Props> = React.forwardRef((props, ref) => {
 
-export interface InputProps {
-  children: ReactNode;
-}
+    const {name,  type, placeholder,  ...rest} = props
 
-function InputIcon(props: InputProps) {
-  return (
-    <Slot className='w-5 h-5 text-gray-400'>
-      {props.children}
-    </Slot>
-    
-  )
-}
+    return(
+        <div >
+           
+            <input  
+                type={type} 
+                name={name}
+                placeholder={placeholder ?? ""}
+                ref={ref}
+                />
+            </div>
+    )
+})
 
-
-export const TextInput = {
-  Root: InputRoot,
-  Input: Input,
-  Icon: InputIcon,
-
-}
+export default Input
